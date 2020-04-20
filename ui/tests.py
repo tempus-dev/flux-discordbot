f482437cea3481d6ca073f4d8966ce7937a92cfe
Priultimus <libertsxion@gmail.com>
2020-04-20 02:38:06 -0400

    Update tests.py

--- ui/tests.py
+++ ui/tests.py
@@ -2,7 +2,11 @@ from discord.ext import commands
 
 
 class Tests(commands.Cog):
-    pass  # I generally put random commands I'm testing here as part of development.
+    # I generally put random commands I'm testing here as part of development.
+
+    @commands.command()
+    async def test(self, ctx):
+        raise NotImplementedError
 
 
 def setup(bot):
