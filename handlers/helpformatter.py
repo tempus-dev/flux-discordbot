49212c29ae3405d94124c97b05bf909872b2a283
Priultimus <libertsxion@gmail.com>
2020-05-06 22:20:30 -0400

    Add local paginator and d.py's old helpformatter

---
+++ handlers/helpformatter.py
@@ -0,0 +1,194 @@
+import itertools
+import inspect
+from discord.ext.commands.core import GroupMixin, Command
+from discord.ext.commands.errors import CommandError
+from discord.ext.commands.help import Paginator
+
+
+class HelpFormatter:
+    """The default base implementation that handles formatting of the help
+    command.
+    To override the behaviour of the formatter, :meth:`~.HelpFormatter.format`
+    should be overridden. A number of utility functions are provided for use
+    inside that method.
+    """
+
+    def __init__(self, show_hidden=False, show_check_failure=False, width=80):
+        self.width = width
+        self.show_hidden = show_hidden
+        self.show_check_failure = show_check_failure
+
+    def has_subcommands(self):
+        """Specifies if the command has subcommands."""
+        return isinstance(self.command, GroupMixin)
+
+    def is_bot(self):
+        """Specifies if the command being formatted is the bot itself."""
+        return self.command is self.context.bot
+
+    def is_cog(self):
+        """Specifies if the command being formatted is actually a cog."""
+        return not self.is_bot() and not isinstance(self.command, Command)
+
+    def shorten(self, text):
+        """Shortens text to fit into the :attr:`width`."""
+        if len(text) > self.width:
+            return text[:self.width - 3] + '...'
+        return text
+
+    @property
+    def max_name_size(self):
+        """Returns the largest name length of a command or if it has subcommands
+        the largest subcommand name."""
+        try:
+            commands = self.command.all_commands if not self.is_cog() else \
+                self.context.bot.all_commands
+            if commands:
+                m = max(map(lambda c: len(c.name) if
+                            self.show_hidden or not c.hidden else
+                            0, commands.values()))
+                return m
+            return 0
+        except AttributeError:
+            return len(self.command.name)
+
+    @property
+    def clean_prefix(self):
+        """The cleaned up invoke prefix.
+
+        i.e. mentions are ``@name`` instead of ``<@id>``."""
+        user = self.context.bot.user
+        # this breaks if the prefix mention is not the bot itself but I
+        # consider this to be an *incredibly* strange use case. I'd rather go
+        # for this common use case rather than waste performance for the
+        # odd one.
+        clean_prefix = self.context.prefix.replace("!", "")
+        clean_prefix = clean_prefix.replace(user.mention, '@' + user.name)
+        return clean_prefix
+
+    def get_command_signature(self):
+        """Retrieves the signature portion of the help page."""
+        prefix = self.clean_prefix
+        cmd = self.command
+        return prefix + cmd.signature
+
+    def get_ending_note(self):
+        command_name = self.context.invoked_with
+        return "Type {0}{1} command for more info on a command.\n" \
+               "You can also type {0}{1} category for more info on a category.".format(  # noqa: E501
+                   self.clean_prefix, command_name)
+
+    async def filter_command_list(self):
+        """Returns a filtered list of commands based on the two attributes
+        provided, `show_check_failure` and `show_hidden`.
+        Also filters based on if `HelpFormatter.is_cog` is valid.
+        """
+
+        def sane_no_suspension_point_predicate(tup):
+            cmd = tup[1]
+            if self.is_cog():
+                # filter commands that don't exist to this cog.
+                if cmd.instance is not self.command:
+                    return False
+
+            if cmd.hidden and not self.show_hidden:
+                return False
+
+            return True
+
+        async def predicate(tup):
+            if sane_no_suspension_point_predicate(tup) is False:
+                return False
+
+            cmd = tup[1]
+            try:
+                return (await cmd.can_run(self.context))
+            except CommandError:
+                return False
+
+        iterator = self.command.all_commands.items() if not self.is_cog(
+        ) else self.context.bot.all_commands.items()
+        if self.show_check_failure:
+            return filter(sane_no_suspension_point_predicate, iterator)
+
+        # Gotta run every check and verify it
+        ret = []
+        for elem in iterator:
+            valid = await predicate(elem)
+            if valid:
+                ret.append(elem)
+
+        return ret
+
+    def _add_subcommands_to_page(self, max_width, commands):
+        for name, command in commands:
+            if name in command.aliases:
+                # skip aliases
+                continue
+
+            entry = '  {0:<{width}} {1}'.format(
+                name, command.short_doc, width=max_width)
+            shortened = self.shorten(entry)
+            self._paginator.add_line(shortened)
+
+    async def format_help_for(self, context, command_or_bot):
+        """Formats the help page and handles the actual heavy lifting of how
+        the help command looks like. To change the behaviour, override the
+        `HelpFormatter.format` method.
+        """
+        self.context = context
+        self.command = command_or_bot
+        return (await self.format())
+
+    async def format(self):
+        """Handles the actual behaviour involved with formatting.
+        To change the behaviour, this method should be overridden.
+        """
+        self._paginator = Paginator(prefix='', suffix='')
+
+        # we need a padding of ~80 or so
+
+        description = self.command.description if not self.is_cog(
+        ) else inspect.getdoc(self.command)
+
+        if description:
+            # <description> portion
+            self._paginator.add_line(description, empty=True)
+
+        if isinstance(self.command, Command):
+
+            # <long doc> section
+            if self.command.help:
+                self._paginator.add_line(self.command.help, empty=True)
+
+            # end it here if it's just a regular command
+            if not self.has_subcommands():
+                self._paginator.close_page()
+                return self._paginator.pages
+
+        max_width = self.max_name_size
+
+        def category(tup):
+            cog = tup[1].cog_name
+            # we insert the zero width space there to give it approximate
+            # last place sorting position.
+            return cog + ':' if cog is not None else '\u200bNo Category:'
+
+        filtered = await self.filter_command_list()
+        if self.is_bot():
+            data = sorted(filtered, key=category)
+            for category, commands in itertools.groupby(data, key=category):
+                # there simply is no prettier way of doing this.
+                commands = sorted(commands)
+                if len(commands) > 0:
+                    self._paginator.add_line(category)
+
+                self._add_subcommands_to_page(max_width, commands)
+        else:
+            filtered = sorted(filtered)
+            if filtered:
+                self._add_subcommands_to_page(max_width, filtered)
+
+        # add the ending note
+        self._paginator.add_line()
+        return self._paginator.pages
---
+++ handlers/paginator.py
@@ -0,0 +1,308 @@
+import asyncio
+import discord
+
+
+class CannotPaginate(Exception):
+    pass
+
+
+class Pages:
+    """Implements a paginator that queries the user for the
+    pagination interface.
+
+    Pages are 1-index based, not 0-index based.
+
+    If the user does not reply within 2 minutes, the pagination
+    interface exits automatically.
+    """
+
+    def __init__(self, bot, *, message, entries, per_page=10):
+        self.bot = bot
+        self.entries = entries
+        self.message = message
+        self.author = message.author
+        self.per_page = per_page
+        pages, left_over = divmod(len(self.entries), self.per_page)
+        if left_over:
+            pages += 1
+        self.maximum_pages = pages
+        self.embed = discord.Embed(
+            color=message.author.color) if message.guild else \
+            discord.Embed(color=0xff0000)
+        self.paginating = len(entries) > per_page
+        self.reaction_emojis = [
+            ('\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}',
+             self.first_page),
+            ('\N{BLACK LEFT-POINTING TRIANGLE}', self.previous_page),
+            ('\N{BLACK RIGHT-POINTING TRIANGLE}', self.next_page),
+            ('\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}',
+             self.last_page),
+            ('\N{INPUT SYMBOL FOR NUMBERS}', self.numbered_page),
+            ('\N{BLACK SQUARE FOR STOP}', self.stop_pages),
+            ('\N{INFORMATION SOURCE}', self.show_help),
+        ]
+
+        server = self.message.guild
+        if server is not None:
+            self.permissions = self.message.channel.permissions_for(server.me)
+        else:
+            self.permissions = self.message.channel.permissions_for(
+                self.bot.user)
+
+        if not self.permissions.embed_links:
+            raise CannotPaginate('Bot does not have embed links permission.')
+
+    def get_page(self, page):
+        base = (page - 1) * self.per_page
+        return self.entries[base:base + self.per_page]
+
+    async def show_page(self, page, *, first=False):
+        self.current_page = page
+        entries = self.get_page(page)
+        p = []
+        for t in enumerate(entries, 1 + ((page - 1) * self.per_page)):
+            p.append(f'{t}. {t}')
+
+        self.embed.set_footer(text=f"Page {page}/{self.maximum_pages}"
+                                   f" ({len(self.entries)} entries)")
+
+        if not self.paginating:
+            self.embed.description = '\n'.join(p)
+            return await self.message.channel.send(embed=self.embed)
+
+        if not first:
+            self.embed.description = '\n'.join(p)
+            try:
+                await self.message.edit(embed=self.embed)
+            except discord.NotFound:
+                self.paginating = False
+            return
+
+        # verify we can actually use the pagination session
+        if not self.permissions.add_reactions:
+            raise CannotPaginate('Bot does not have add reactions permission.')
+
+        if not self.permissions.read_message_history:
+            raise CannotPaginate('Bot does not have Read Message'
+                                 ' History permission.')
+
+        p.append('')
+        p.append('Confused? React with \N{INFORMATION SOURCE} for more info.')
+        self.embed.description = '\n'.join(p)
+        self.message = await self.message.channel.send(embed=self.embed)
+        for (reaction, _) in self.reaction_emojis:
+            if self.maximum_pages == 2 and reaction in ('\u23ed', '\u23ee'):
+                # no |<< or >>| buttons if we only have two pages
+                # we can't forbid it if someone ends up using it but remove
+                # it from the default set
+                continue
+            try:
+                await self.message.add_reaction(reaction)
+            except discord.NotFound:
+                # If the message isn't found, we don't care about clearing
+                # anything
+                return
+
+    async def checked_show_page(self, page):
+        if page != 0 and page <= self.maximum_pages:
+            await self.show_page(page)
+
+    async def first_page(self):
+        """goes to the first page"""
+        await self.show_page(1)
+
+    async def last_page(self):
+        """goes to the last page"""
+        await self.show_page(self.maximum_pages)
+
+    async def next_page(self):
+        """goes to the next page"""
+        await self.checked_show_page(self.current_page + 1)
+
+    async def previous_page(self):
+        """goes to the previous page"""
+        await self.checked_show_page(self.current_page - 1)
+
+    async def show_current_page(self):
+        if self.paginating:
+            await self.show_page(self.current_page)
+
+    async def numbered_page(self):
+        """lets you type a page number to go to"""
+        to_delete = []
+        channel = self.message.channel
+        to_delete.append(await channel.send('What page do you want to go to?'))
+
+        def check(m):
+            if m.author == self.author and m.channel == self.message.channel:
+                return m.content.isdigit()
+            else:
+                return False
+
+        try:
+            msg = await self.bot.wait_for('message', check=check, timeout=30.0)
+        except asyncio.TimeoutError:
+            msg = None
+        if msg is not None:
+            page = int(msg.content)
+            to_delete.append(msg)
+            if page != 0 and page <= self.maximum_pages:
+                await self.show_page(page)
+            else:
+                to_delete.append(await self.message.channel.send(
+                    f'Invalid page given. ({page}/{self.maximum_pages})'))
+                await asyncio.sleep(5)
+        else:
+            to_delete.append(await self.message.channel.send('Took too long.'))
+            await asyncio.sleep(5)
+
+        try:
+            await self.message.channel.delete_messages(to_delete)
+        except Exception:
+            pass
+
+    async def show_help(self):
+        """shows this message"""
+        e = discord.Embed()
+        messages = ['Welcome to the interactive paginator!\n',
+                    'This interactively allows you to see pages '
+                    'of text by navigating with '
+                    'reactions. They are as follows:\n']
+
+        for (emoji, func) in self.reaction_emojis:
+            messages.append(f'{emoji} {func.__doc__}')
+
+        e.description = '\n'.join(messages)
+        e.colour = 0x738bd7  # blurple
+        curn = self.current_page
+        e.set_footer(text=f"We were on page {curn} before this message.")
+        await self.message.edit(embed=e)
+
+        async def go_back_to_current_page():
+            await asyncio.sleep(60.0)
+            await self.show_current_page()
+
+        self.bot.loop.create_task(go_back_to_current_page())
+
+    async def stop_pages(self):
+        """stops the interactive pagination session"""
+        await self.message.delete()
+        self.paginating = False
+
+    def react_check(self, reaction, user):
+        if user is None or user.id != self.author.id or \
+                reaction.message.id != self.message.id:
+            return False
+
+        for (emoji, func) in self.reaction_emojis:
+            if reaction.emoji == emoji:
+                self.match = func
+                return True
+        return False
+
+    async def paginate(self, start_page=1):
+        """Actually paginate the entries and run the interactive loop if necessary."""  # noqa: E501
+        await self.show_page(start_page, first=True)
+
+        while self.paginating:
+            try:
+                reaction_add = self.bot.wait_for('reaction_add',
+                                                 check=self.react_check,
+                                                 timeout=120.0)
+                reaction_add = asyncio.ensure_future(reaction_add)
+                reaction_remove = self.bot.wait_for('reaction_remove',
+                                                    check=self.react_check,
+                                                    timeout=120.0)
+                reaction_remove = asyncio.ensure_future(reaction_remove)
+                aws = {reaction_add, reaction_remove}
+                constant = 'FIRST_COMPLETED'
+                completed, pending = await asyncio.wait(aws,
+                                                        return_when=constant)
+                for first_completed in completed:
+                    react, user = first_completed.result()
+                    break
+            except asyncio.TimeoutError:
+                self.paginating = False
+                try:
+                    await self.message.clear_reactions()
+                except discord.errors.Forbidden:
+                    pass
+                finally:
+                    break
+
+            try:
+                await self.message.remove_reaction(react.emoji, user)
+            except discord.errors.Forbidden:
+                pass  # can't remove it so don't bother doing so
+
+            await self.match()
+
+
+class DetailedPages(Pages):
+    """A class built on the normal Paginator, except with the idea
+    that you want one 'thing' per page. This allows the ability to have
+    more data on a page, more fields, etc. and page through each 'thing'"""
+
+    def __init__(self, *args, **kwargs):
+        kwargs['per_page'] = 1
+        super().__init__(*args, **kwargs)
+
+    def get_page(self, page):
+        return self.entries[page - 1]
+
+    async def show_page(self, page, *, first=False):
+        self.current_page = page
+        entries = self.get_page(page)
+
+        self.embed.set_footer(text='Page %s/%s (%s entries)' %
+                              (page, self.maximum_pages, len(self.entries)))
+        self.embed.clear_fields()
+        self.embed.description = ""
+
+        for key, value in entries.items():
+            if key == 'fields':
+                for f in value:
+                    self.embed.add_field(name=f.get('name'),
+                                         value=f.get('value'),
+                                         inline=f.get('inline', True))
+            else:
+                setattr(self.embed, key, value)
+
+        if not self.paginating:
+            return await self.message.channel.send(embed=self.embed)
+
+        if not first:
+            try:
+                await self.message.edit(embed=self.embed)
+            except discord.NotFound:
+                self.paginating = False
+            return
+
+        # verify we can actually use the pagination session
+        if not self.permissions.add_reactions:
+            raise CannotPaginate('Bot does not have add reactions permission.')
+
+        if not self.permissions.read_message_history:
+            raise CannotPaginate('Bot does not have Read Message '
+                                 'History permission.')
+        info_emoji = "\N{INFORMATION SOURCE}"
+        if self.embed.description:
+            self.embed.description += '\nConfused? '
+            self.embed.description += f'React with {info_emoji} for more info.'
+        else:
+            self.embed.description = f'\nConfused? '
+            self.embed.description += f'React with {info_emoji} for more info.'
+
+        self.message = await self.message.channel.send(embed=self.embed)
+        for (reaction, _) in self.reaction_emojis:
+            if self.maximum_pages == 2 and reaction in ('\u23ed', '\u23ee'):
+                # no |<< or >>| buttons if we only have two pages
+                # we can't forbid it if someone ends up using it but remove
+                # it from the default set
+                continue
+            try:
+                await self.message.add_reaction(reaction)
+            except discord.NotFound:
+                # If the message isn't found, we don't care about clearing
+                # anything
+                return
