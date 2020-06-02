ab70310895337df67fc60a45880196335eddf9f6
Critiql <46457297+Critiql@users.noreply.github.com>
2020-06-02 19:08:50 +0100

    use consistent error messages

--- ui/general.py
+++ ui/general.py
@@ -110,13 +110,13 @@ To use the interactive help menu use the reactions:
     @commands.command(name="points")
     async def points(self, ctx, user: discord.Member = None) -> None:
         """Check yours or someone else's points."""
-      if not ctx.bot.db_client:
-          await ctx.send("Without the database running, this command"
+       if not ctx.bot.db_client:
+           await ctx.send("Without the database running, this command"
                            " is defunct. "
                            "Please use `.contact` with error:"
                            " `ERR_CONN_FAILURE`"
                            )
-          return
+            return
 
         if not user:
             user = ctx.author
